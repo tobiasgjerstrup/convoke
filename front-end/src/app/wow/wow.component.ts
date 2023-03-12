6f99c259787563867d6b00e747c6f26384d00f77
tobiasgjerstrup <58333752+tobiasgjerstrup@users.noreply.github.com>
2023-03-12 16:15:46 +0100

    wow item search can now show 10k results

--- front-end/src/app/wow/wow.component.ts
+++ front-end/src/app/wow/wow.component.ts
@@ -14,13 +14,24 @@ export class WowComponent {
   constructor(private http: HttpClient, public toastService: ToastService) { }
 
   search(event: any) {
-    this.http.get<any>('https://convoke.uk/api/v1/$limit=1000$order=asc$search=' + event.target.value).subscribe(data => {
-      this.items = data
-      if (this.items.length > 0)
-        this.showSuccess("Found " + this.items.length + " Items")
+    this.http.get<any>('https://convoke.uk/api/v1/$count=true$search=' + event.target.value).subscribe(count => {
+      console.log(count[0].count)
+      if (count[0].count > 10000) {
+        this.showDanger("Found " + count[0].count + " Items" + " Limit is 10000")
+        this.showSuccess("Showing 10000 Items")
+      } else if (count[0].count > 0)
+        this.showSuccess("Found " + count[0].count + " Items")
       else
         this.showDanger("Your search gave 0 results")
 
+      this.http.get<any>('https://convoke.uk/api/v1/$limit=1000$order=asc$search=' + event.target.value).subscribe(data => {
+        this.items = data
+        if (count[0].count > 1000) {
+          this.http.get<any>('https://convoke.uk/api/v1/$limit=9000$order=asc$offset=1000$search=' + event.target.value).subscribe(extraData => {
+            this.items = this.items.concat(extraData)
+          });
+        }
+      });
     });
   }
   showSuccess(message: string) {
