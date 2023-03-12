bf36ad2af23cf61344d36dcda767ece529bc2ace
tobiasgjerstrup <58333752+tobiasgjerstrup@users.noreply.github.com>
2023-03-12 22:06:53 +0100

    added a commit history table that automatically updates

---
+++ scripts/commitsGet.js
@@ -0,0 +1,39 @@
+import mysql from 'mysql2';
+import { database } from './config.js';
+import axios from 'axios';
+
+const connection = mysql.createConnection(database).promise()
+
+await connection.query("delete from gitCommits")
+
+const getCommits = () => axios.get(' https://api.github.com/repos/tobiasgjerstrup/convoke/commits', {})
+    .then(function (response) {
+        let commits = []
+        for (var i = 0; i < response.data.length; i++) {
+            commits.push(response.data[i].url)
+        }
+        return commits
+    });
+
+const getCommitData = (url) => axios.get(url, {})
+    .then(function (response) {
+        return {
+            name: response.data.commit.committer.name,
+            date: response.data.commit.committer.date,
+            message: response.data.commit.message,
+            url: response.data.html_url,
+            deletions: response.data.stats.deletions,
+            additions: response.data.stats.additions,
+            changed_files: response.data.files.length
+        }
+    });
+
+
+const res = await getCommits();
+
+for (var i = 0; i < res.length; i++) {
+    let data = await getCommitData(res[i])
+    await connection.query("insert into gitCommits values ('" + data.name + "', '" + data.date.replace("T", " ").replace("Z", "") + "', '" + data.message + "', '" + data.url + "', " + data.additions + ", " + data.deletions + ", " + data.changed_files + ")")
+}
+
+connection.destroy();
