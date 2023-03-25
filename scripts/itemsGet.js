c0e080aeb1e4628977a876f4799b86b707c91b46
= <58333752+tobiasgjerstrup@users.noreply.github.com>
2023-03-25 20:35:12 +0100

    Updated itemsGet

--- scripts/itemsGet.js
+++ scripts/itemsGet.js
@@ -1,41 +1,20 @@
-import convert from "xml-js";
-import axios from "axios";
-import mysql from "mysql2";
-import { database } from "./config.js";
+import * as libs from "./libs.js";
 
-const connection = mysql.createConnection(database).promise();
-
-async function insertValues(id, name, icon, timestamp) {
-  console.log("DELETE FROM items WHERE id = " + id + ";");
-  await connection.query("DELETE FROM items WHERE id = " + id + ";");
-  console.log("insert into items values (" + id + ', "' + name + '", "' + icon + '", "' + timestamp + '")');
-  await connection.query("insert into items values (" + id + ', "' + name + '", "' + icon + '", "' + timestamp + '")');
-  return true;
-}
-async function sqlQuery(query) {
-  const result = await connection.query(query);
-  return result[0];
-}
-
-const wowheadcall = (i) =>
-  axios.get("https://www.wowhead.com/item=" + i + "&xml", {}).then(function (response) {
-    const string = convert.xml2json(response.data, {
-      compact: true,
-      space: 4,
-    });
-    const json = JSON.parse(string);
-    if (json.wowhead.hasOwnProperty("error")) return;
-    insertValues(json.wowhead.item._attributes.id, json.wowhead.item.name._cdata.replaceAll('"', "'"), json.wowhead.item.icon._text, new Date().toISOString().slice(0, 10));
-    console.log("id: " + json.wowhead.item._attributes.id + " name: " + json.wowhead.item.name._cdata.replaceAll('"', "'"));
-  });
-
-const value = await sqlQuery("SELECT * from items ORDER BY id desc LIMIT 1");
+const value = await libs.sqlQuery("SELECT * from items ORDER BY updated LIMIT 1");
 console.log(value);
 if (!Object.entries(value).length) {
-  for (var i = 0; i < 3000000; i++) {
-    const res = await wowheadcall(i);
+  for (let i = 0; i < 3000000; i++) {
+    const res = await libs.getWoWHeadXMLAsJSON("https://www.wowhead.com/item=" + i + "&xml");
+    if (res) {
+      libs.deleteAndInsert(res.wowhead.item._attributes.id, res.wowhead.item.name._cdata.replaceAll('"', "'"), res.wowhead.item.icon._text, new Date().toISOString().slice(0, 10));
+      console.log(res.wowhead.item._attributes.id + " " + res.wowhead.item.name._cdata);
+    }
   }
 }
-for (var i = value[0].id; i < 3000000; i++) {
-  const res = await wowheadcall(i);
+for (let i = value[0].id; i < 3000000; i++) {
+  const res = await libs.getWoWHeadXMLAsJSON("https://www.wowhead.com/item=" + i + "&xml");
+  if (res) {
+    libs.deleteAndInsert(res.wowhead.item._attributes.id, res.wowhead.item.name._cdata.replaceAll('"', "'"), res.wowhead.item.icon._text, new Date().toISOString().slice(0, 10));
+    console.log(res.wowhead.item._attributes.id + " " + res.wowhead.item.name._cdata);
+  }
 }
--- scripts/libs.js
+++ scripts/libs.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 import fs from "fs";
+import { database } from "./config.js";
+import mysql from "mysql2";
+import convert from "xml-js";
+
+const connection = mysql.createConnection(database).promise();
 
 export function sleep(ms) {
   return new Promise((resolve) => {
@@ -21,4 +26,29 @@ export async function downloadImage(url, path) {
   const writer = fs.createWriteStream(path);
   response.data.pipe(writer);
   return response.data;
+}
+
+export async function deleteAndInsert(id, name, icon, timestamp) {
+  await connection.query("DELETE FROM items WHERE id = " + id + ";");
+  await connection.query("insert into items values (" + id + ', "' + name + '", "' + icon + '", "' + timestamp + '")');
+  return true;
+}
+
+export async function getWoWHeadXMLAsJSON(url) {
+  const response = await axios({
+    url,
+    method: "GET",
+  });
+  const string = convert.xml2json(response.data, {
+    compact: true,
+    space: 4,
+  });
+  const json = JSON.parse(string);
+  if (json.wowhead.hasOwnProperty("error")) return false;
+  return json;
+}
+
+export async function sqlQuery(query) {
+  const result = await connection.query(query);
+  return result[0];
 }
