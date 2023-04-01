fe0d058863b79bb4973486f51395215bb664b072
= <58333752+tobiasgjerstrup@users.noreply.github.com>
2023-04-01 16:40:26 +0200

    updated syncTest to log more and include git commits

--- scripts/syncTest.js
+++ scripts/syncTest.js
@@ -1,12 +1,25 @@
 import * as libs from "./libs.js";
 
-const data = await libs.select("production", "items");
+// GET DATA FROM PRODUCTION
+const itemsData = await libs.select("production", "items");
+const gitCommitsData = await libs.select("production", "gitCommits")
 
+// DELETE DATA FROM TEST
 await libs.clearTable('test', 'items')
+await libs.clearTable('test', 'gitCommits')
 
-for (let i = 0; i < data.length; i++) {
-  await libs.insert('test', "items", data[i].id, data[i].name, data[i].icon, data[i].updated, data[i].class, data[i].subclass);
+// SEED TEST WITH DATA FROM PRODUCTION
+// items
+for (let i = 0; i < itemsData.length; i++) {
+  await libs.insert('test', "items", itemsData[i].id, itemsData[i].name, itemsData[i].icon, itemsData[i].updated, itemsData[i].class, itemsData[i].subclass);
+  console.log(new Date().toISOString().slice(0, 19) + ' Inserted: ' + itemsData[i].id + ' ' + itemsData[i].name)
+}
+// gitCommits
+for (let i = 0; i < gitCommitsData.length; i++) {
+  await libs.insert('test', "items", gitCommitsData[i].name, gitCommitsData[i].date, gitCommitsData[i].message, gitCommitsData[i].url, gitCommitsData[i].additions, gitCommitsData[i].deletions, gitCommitsData[i].changed_files);
+  console.log(new Date().toISOString().slice(0, 19) + ' Inserted: ' + gitCommitsData[i].message + ' ' + gitCommitsData[i].url)
 }
 
+// FINISH SCRIPT
 console.log("script finished");
 process.exit(0);
