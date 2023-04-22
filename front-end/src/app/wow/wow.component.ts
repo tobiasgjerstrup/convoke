e27a5b2d5b333c0be0182a6750171758c5d317b0
= <58333752+tobiasgjerstrup@users.noreply.github.com>
2023-04-22 17:16:00 +0200

    changed colour of items in the wow item search page, depending on quality

--- front-end/src/app/wow/wow.component.ts
+++ front-end/src/app/wow/wow.component.ts
@@ -6,38 +6,51 @@ import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-wow',
   templateUrl: './wow.component.html',
-  styleUrls: ['./wow.component.scss']
+  styleUrls: ['./wow.component.scss'],
 })
 export class WowComponent {
+  items = [{ id: null, name: null, icon: null, quality: 0 }];
 
-  items = [{ id: null, name: null, icon: null }]
-
-  constructor(private http: HttpClient, public toastService: ToastService, private _renderer2: Renderer2, @Inject(DOCUMENT) private _document: Document) { }
+  constructor(private http: HttpClient, public toastService: ToastService, private _renderer2: Renderer2, @Inject(DOCUMENT) private _document: Document) {}
 
   public ngOnInit() {
-
     //load scripts
     let script = this._renderer2.createElement('script');
-    script.src = "https://wow.zamimg.com/js/tooltips.js";
+    script.src = 'https://wow.zamimg.com/js/tooltips.js';
     this._renderer2.appendChild(this._document.body, script);
   }
 
-  search(event: any) {
-    this.http.get<any>('https://convoke.uk/api/v1/$count=true$search=' + event.target.value).subscribe(count => {
-      console.log(count[0].count)
-      if (count[0].count > 10000) {
-        this.showDanger("Found " + count[0].count + " Items" + " Limit is 10000")
-        this.showSuccess("Showing 10000 Items")
-      } else if (count[0].count > 0)
-        this.showSuccess("Found " + count[0].count + " Items")
-      else
-        this.showDanger("Your search gave 0 results")
+  public getQuality(quality: number) {
+    switch (quality) {
+      case 1:
+        return '#ffffff';
+      case 2:
+        return '#1eff00';
+      case 3:
+        return '#0070dd';
+      case 4:
+        return '#a335ee';
+      case 5:
+        return '#ff8000';
+      default:
+        return '#9d9d9d';
+    }
+  }
 
-      this.http.get<any>('https://convoke.uk/api/v1/$limit=1000$order=asc$search=' + event.target.value).subscribe(data => {
-        this.items = data
+  search(event: any) {
+    this.http.get<any>('https://convoke.uk/api/v1/$count=true$search=' + event.target.value).subscribe((count) => {
+      console.log(count[0].count);
+      if (count[0].count > 10000) {
+        this.showDanger('Found ' + count[0].count + ' Items' + ' Limit is 10000');
+        this.showSuccess('Showing 10000 Items');
+      } else if (count[0].count > 0) this.showSuccess('Found ' + count[0].count + ' Items');
+      else this.showDanger('Your search gave 0 results');
+
+      this.http.get<any>('https://convoke.uk/api/v1/$limit=1000$order=asc$search=' + event.target.value).subscribe((data) => {
+        this.items = data;
         if (count[0].count > 1000) {
-          this.http.get<any>('https://convoke.uk/api/v1/$limit=9000$order=asc$offset=1000$search=' + event.target.value).subscribe(extraData => {
-            this.items = this.items.concat(extraData)
+          this.http.get<any>('https://convoke.uk/api/v1/$limit=9000$order=asc$offset=1000$search=' + event.target.value).subscribe((extraData) => {
+            this.items = this.items.concat(extraData);
           });
         }
       });
