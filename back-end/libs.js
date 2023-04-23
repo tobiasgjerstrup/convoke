a631b753efb376bb7c117407057320e5dd21c233
= <58333752+tobiasgjerstrup@users.noreply.github.com>
2023-04-23 10:35:41 +0200

    improved wow items order param & Added a custom sort to site

--- back-end/libs.js
+++ back-end/libs.js
@@ -1,103 +1,112 @@
-import shelljs from 'shelljs';
+import shelljs from "shelljs";
 
 export function getParams(params) {
-    let splitParams = {};
-    for (var i = 1; i < params.length; i++) {
-        let keyvalue = params[i].split('=');
-        splitParams[keyvalue[0]] = keyvalue[1]
-    }
-    return splitParams;
+  let splitParams = {};
+  for (var i = 1; i < params.length; i++) {
+    let keyvalue = params[i].split("=");
+    splitParams[keyvalue[0]] = keyvalue[1];
+  }
+  return splitParams;
 }
 
 export function call(call, params) {
-    if (call[1] !== 'api') { //only accept this as the call
-        return false;
-    }
-    switch (params.table) {
-        case "gitcommits":
-            params.table = 'gitCommits'
-            var res = getGitCommits(params)
-            return res
-            break;
-        default:
-            if (params.webhook) {
-                pull()
-                return 'webhook started'
-            }
-            params.table = 'items'
-            var res = getItems(params)
-            return res
-            break;
-    }
-    return 'error in call function';
+  if (call[1] !== "api") {
+    //only accept this as the call
+    return false;
+  }
+  switch (params.table) {
+    case "gitcommits":
+      params.table = "gitCommits";
+      var res = getGitCommits(params);
+      return res;
+      break;
+    default:
+      if (params.webhook) {
+        pull();
+        return "webhook started";
+      }
+      params.table = "items";
+      var res = getItems(params);
+      return res;
+      break;
+  }
+  return "error in call function";
 }
 
 export function modifyData(value, params) {
-    if (params.table) {
-        if (params.table === 'gitCommits') {
-            value.forEach(element => {
-                element.date = element.date.toLocaleString()
-            });
-        }
+  if (params.table) {
+    if (params.table === "gitCommits") {
+      value.forEach((element) => {
+        element.date = element.date.toLocaleString();
+      });
     }
-    return value
+  }
+  return value;
 }
 
 function getGitCommits(params) {
-    let sql = 'select'
-    if (params.count && params.count === 'true') {
-        sql = sql + ' count(*) from ' + params.table
-    } else {
-        sql = sql + ' * from ' + params.table
+  let sql = "select";
+  if (params.count && params.count === "true") {
+    sql = sql + " count(*) from " + params.table;
+  } else {
+    sql = sql + " * from " + params.table;
+  }
+  if (params.search) {
+    sql = sql + ' where name like "%' + params.search + '%"';
+  }
+  if (params.order) {
+    if (params.order === "desc") {
+      sql = sql + " order by message desc";
+    } else if (params.order === "asc") {
+      sql = sql + " order by message asc";
+    } else if (params.order === "date") {
+      sql = sql + " order by date";
     }
-    if (params.search) {
-        sql = sql + ' where name like "%' + params.search + '%"';
+  }
+  if (params.limit) {
+    sql = sql + " limit " + params.limit;
+    if (params.offset) {
+      sql = sql + " offset " + params.offset;
     }
-    if (params.order) {
-        if (params.order === 'desc') {
-            sql = sql + ' order by message desc'
-        } else if (params.order === 'asc') {
-            sql = sql + ' order by message asc'
-        } else if (params.order === 'date') {
-            sql = sql + ' order by date'
-        }
-    }
-    if (params.limit) {
-        sql = sql + ' limit ' + params.limit
-        if (params.offset) {
-            sql = sql + ' offset ' + params.offset
-        }
-    }
-    return sql;
+  }
+  return sql;
 }
 function getItems(params) {
-    let sql = 'select'
-    if (params.count && params.count === 'true') {
-        sql = sql + ' count(*) from ' + params.table
+  let sql = "select";
+  if (params.count && params.count === "true") {
+    sql = sql + " count(*) from " + params.table;
+  } else {
+    sql = sql + " * from " + params.table;
+  }
+  if (params.search) {
+    sql = sql + ' where name like "%' + params.search + '%"';
+  }
+  if (params.order) {
+    let order = "";
+    let ascDesc = "";
+    if (params.order.includes("-")) {
+      order = params.order.split("-")[0];
+      ascDesc = params.order.split("-")[1];
     } else {
-        sql = sql + ' * from ' + params.table
+      order = params.order;
     }
-    if (params.search) {
-        sql = sql + ' where name like "%' + params.search + '%"';
+
+    if (order === "id" || order === "name" || order === "icon" || order === "class" || order === "subclass" || order === "quality" || order === "last_update" || order === "created") {
+      sql = sql + ` order by ${order}`;
+      if ((ascDesc === "desc")) {
+        sql = sql + " desc";
+      }
     }
-    if (params.order) {
-        if (params.order === 'desc') {
-            sql = sql + ' order by id desc'
-        } else if (params.order === 'asc') {
-            sql = sql + ' order by id asc'
-        } else if (params.order === 'lastupdated') {
-            sql = sql + ' order by updated'
-        }
+  }
+  if (params.limit) {
+    sql = sql + " limit " + params.limit;
+    if (params.offset) {
+      sql = sql + " offset " + params.offset;
     }
-    if (params.limit) {
-        sql = sql + ' limit ' + params.limit
-        if (params.offset) {
-            sql = sql + ' offset ' + params.offset
-        }
-    }
-    return sql;
+  }
+  return sql;
 }
 
 function pull() {
-    shelljs.exec('git pull; pm2 restart database')
+  shelljs.exec("git pull; pm2 restart database");
 }
--- front-end/src/app/wow/wow.component.ts
+++ front-end/src/app/wow/wow.component.ts
@@ -10,7 +10,11 @@ import { DOCUMENT } from '@angular/common';
 })
 export class WowComponent {
   items = [{ id: null, name: null, icon: null, quality: 0 }];
+  apiSite = 'http://localhost:8080'; // 'https://convoke.uk' 'http://localhost:8080'
+  order = 'id';
 
+  maxResults = 2000;
+  lazyResults = 1000; //amount of results to load after the initial results has loaded. Initial amount is maxResults - lazyResults
   constructor(private http: HttpClient, public toastService: ToastService, private _renderer2: Renderer2, @Inject(DOCUMENT) private _document: Document) {}
 
   public ngOnInit() {
@@ -32,24 +36,32 @@ export class WowComponent {
         return '#a335ee';
       case 5:
         return '#ff8000';
+      case 6:
+        return '#e5cc80';
+      case 7:
+        return '#00ccff';
+      case 8:
+        return '#00ccff';
       default:
         return '#9d9d9d';
     }
   }
-
+  updateOrder(event: any) {
+    this.order = event.target.value;
+  }
   search(event: any) {
-    this.http.get<any>('https://convoke.uk/api/v1/?count=true&search=' + event.target.value).subscribe((count) => {
+    this.http.get<any>(this.apiSite + '/api/v1/?count=true&search=' + event.target.value).subscribe((count) => {
       console.log(count[0].count);
-      if (count[0].count > 10000) {
-        this.showDanger('Found ' + count[0].count + ' Items' + ' Limit is 10000');
-        this.showSuccess('Showing 10000 Items');
+      if (count[0].count > this.maxResults) {
+        this.showDanger('Found ' + count[0].count + ' Items' + ' Limit is ' + this.maxResults);
+        this.showSuccess('Showing ' + this.maxResults + ' Items');
       } else if (count[0].count > 0) this.showSuccess('Found ' + count[0].count + ' Items');
       else this.showDanger('Your search gave 0 results');
-
-      this.http.get<any>('https://convoke.uk/api/v1/?limit=1000&order=asc&search=' + event.target.value).subscribe((data) => {
+      const initResults = this.maxResults - this.lazyResults;
+      this.http.get<any>(this.apiSite + '/api/v1/?limit=' + initResults + '&order=' + this.order + '&search=' + event.target.value).subscribe((data) => {
         this.items = data;
-        if (count[0].count > 1000) {
-          this.http.get<any>('https://convoke.uk/api/v1/?limit=9000&order=asc&offset=1000&search=' + event.target.value).subscribe((extraData) => {
+        if (count[0].count > initResults) {
+          this.http.get<any>(this.apiSite + '/api/v1/?limit=' + this.lazyResults + '&order=' + this.order + '&offset=' + initResults + '&search=' + event.target.value).subscribe((extraData) => {
             this.items = this.items.concat(extraData);
           });
         }
