7642530720a79ced340836aea0de3d79309ef82a
= <58333752+tobiasgjerstrup@users.noreply.github.com>
2023-05-12 18:00:40 +0200

    quickfix

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrl: 'http://localhost:8080/'
-    // apiUrl: 'https://convoke.uk/'
+    // apiUrl: 'http://localhost:8080/'
+    apiUrl: 'https://convoke.uk/'
 };
