4aee8299aec66100fbc128eaf4b4186dc4bf4775
= <58333752+tobiasgjerstrup@users.noreply.github.com>
2023-06-08 19:15:08 +0200

    discordbot can now play music

--- discordbot/discordbot.js
+++ discordbot/discordbot.js
@@ -1,16 +1,18 @@
 import dotenv from "dotenv";
 import { Client, GatewayIntentBits } from "discord.js";
-import { joinVoiceChannel } from "@discordjs/voice";
+import { joinVoiceChannel, createAudioResource, createAudioPlayer } from "@discordjs/voice";
 import * as libs from "../scripts/libs.js";
+import downloadFromYoutube from "../scripts/youtube-to-mp3/index.js";
 
 dotenv.config();
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildVoiceStates],
 });
 
 client.login(process.env.DISCORD_TOKEN);
 
 let connection = "";
+const player = createAudioPlayer();
 client.on("messageCreate", async (message) => {
   if (!message?.author.bot) {
     if (message.content.toLowerCase() === "convokejoin") {
@@ -24,13 +26,26 @@ client.on("messageCreate", async (message) => {
       if (connection && connection._state.status !== "destroyed") connection.destroy();
     }
     if (message.content.toLowerCase() === "convokefact") {
-      const data = await libs.select('production', 'convokefacts');
+      const data = await libs.select("production", "convokefacts");
       const convokeFactMessageID = Math.floor(Math.random() * data.length);
-      const convokeFactRealID = convokeFactMessageID+1
+      const convokeFactRealID = convokeFactMessageID + 1;
       message.channel.send(data[convokeFactMessageID].fact + " [message " + convokeFactRealID + "/" + data.length + "]");
     }
     if (message.content.toLowerCase().startsWith("convokefactadd ")) {
-      await libs.insert('production', 'convokefacts', `${message.author.username}`, `${message.content.slice(15)}`);
+      await libs.insert("production", "convokefacts", `${message.author.username}`, `${message.content.slice(15)}`);
+    }
+    if (message.content.toLowerCase().startsWith("convokeplay ")) {
+      const YOUTUBE_URL = message.content.slice(12);
+      const OUTPUT = "media/mp3/" + YOUTUBE_URL.split("watch?v=").pop() + ".mp3";
+      await downloadFromYoutube(YOUTUBE_URL, OUTPUT);
+      connection = joinVoiceChannel({
+        channelId: message.member.voice.channel.id,
+        guildId: message.guild.id,
+        adapterCreator: message.guild.voiceAdapterCreator,
+      });
+      const resource = createAudioResource(OUTPUT);
+      player.play(resource);
+      connection.subscribe(player);
     }
   }
 });
