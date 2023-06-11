92121bb7d03e7983f56bfadf4a9a9bb6a9e78ca2
= <58333752+tobiasgjerstrup@users.noreply.github.com>
2023-06-11 13:53:19 +0200

    removed some console logs from itemsGet

--- scripts/itemsGet.js
+++ scripts/itemsGet.js
@@ -25,13 +25,11 @@ async function getAndPush(i) {
         res.wowhead.item.subclass._cdata, // SUBCLASS
         res.wowhead.item.quality._attributes.id // QUALITY
       );
-      console.log("ran deleteAndInsert on: " + res.wowhead.item._attributes.id + " " + res.wowhead.item.name._cdata);
       return true;
     }
 
     // IF DIFFS
     if ((DBData[0].quality !== null) && (DBData[0].name === WoWHeadData.name && DBData[0].icon === WoWHeadData.icon && DBData[0].class === WoWHeadData.class && DBData[0].subclass === WoWHeadData.subclass && DBData[0].quality.toString() === WoWHeadData.quality)) {
-      console.log("this is the same on wowhead & DB: " + res.wowhead.item._attributes.id + " " + res.wowhead.item.name._cdata);
       return true;
     } else {
       await libs.updateTable("production", "items", i, `name = "${WoWHeadData.name}", icon = "${WoWHeadData.icon}", class = "${WoWHeadData.class}", subclass = "${WoWHeadData.subclass}", quality = ${WoWHeadData.quality}`);
@@ -39,7 +37,6 @@ async function getAndPush(i) {
       return true;
     }
   } else {
-    console.log("item with ID " + i + " was not found");
     return false;
   }
 }
