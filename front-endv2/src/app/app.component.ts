19e265f003d0126af0d2a9c46ac51394fb335223
= <58333752+tobiasgjerstrup@users.noreply.github.com>
2023-09-06 18:26:55 +0200

    added a better login screen

--- front-endv2/src/app/app.component.ts
+++ front-endv2/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { httpService } from '../services/http-service.service';
 import { environment } from 'src/environments/environment';
+import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,17 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(private http: httpService) {}
+  constructor(private http: httpService, private modalService: NgbModal, private config: NgbModalConfig) {
+    config.backdrop = 'static';
+		config.keyboard = false;
+  }
+
+  @ViewChild('content2') content2 : any;
+
+  openModal() {
+    this.modalService.open(this.content2);
+  }
+
   title = 'front-endv2';
 
   user = '';
@@ -40,12 +51,18 @@ export class AppComponent {
       .post(environment.apiUrl + 'api/v1/signin', this.loginInfo)
       .subscribe((data) => {
         this.user = data.user;
+        if (this.user){
+          this.modalService.dismissAll('User logged in!');
+        }
       });
   }
 
   ngOnInit() {
     this.http.get(environment.apiUrl + 'api/v1/user').subscribe((data) => {
       this.user = data.user;
+      if (!this.user){
+        this.openModal();
+      }
     });
   }
 }
