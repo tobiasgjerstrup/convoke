bcde4fbd7a08a49005eb18a56b38766382fab48c
= <tobiasgjerstrup@gmail.com>
2023-10-29 08:36:54 +0100

    did some more rusting

--- rust-wep-api/src/api/task.rs
+++ rust-wep-api/src/api/task.rs
@@ -1,16 +1,7 @@
 use actix_web::{
     get,
-    post,
-    put,
-    error::ResponseError,
-    web::Path,
     web::Json,
-    web::Data,
-    HttpResponse,
-    http::{header::ContentType, StatusCode}
 };
-use serde::{Serialize, Deserialize};
-use derive_more::{Display}; 
 
 #[get("/test")]
 pub async fn get_task() -> Json<String>{
--- rust-wep-api/src/main.rs
+++ rust-wep-api/src/main.rs
@@ -1,16 +1,59 @@
 mod api;
-use actix_web::{middleware::Logger, web::Data, App, HttpServer};
+
+use actix_web::{middleware::Logger, App, HttpServer};
 use api::task::{get_task, get_task2};
 use rand::Rng;
-use std::{cmp::Ordering, io};
+use std::{cmp::Ordering, env, io};
 
 #[actix_web::main]
 
-async fn main() -> std::io::Result<()> {
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+async fn main() {
+    // Get input args. Should probably have some input args
+    let args: Vec<_> = env::args().collect();
+    if args.len() <= 1 {
+        println!("no argument found! Try help");
+    } else {
+        run().await;
+    }
+}
 
+async fn run() {
+    let args: Vec<_> = env::args().collect();
+    println!("The first argument is {}", args[1]);
+    if args[1] == "help" {
+        println!("web => webserver");
+        println!("guess => guessing game");
+    } else if args[1] == "web" {
+        let _ = bind_web_server("127.0.0.1", 80).await;
+        println!("something went horribly wrong!");
+    } else if args[1] == "guess" {
+        let attempts = guessing_game();
+        println!("you finished with {attempts} attempts");
+    } else {
+        println!("Invalid argument!");
+    }
+}
+
+async fn bind_web_server(ip: &str, port: u16) -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    std::env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
+
+    HttpServer::new(move || {
+        let logger = Logger::default();
+        App::new().wrap(logger).service(get_task).service(get_task2)
+    })
+    .bind((ip, port))?
+    .run()
+    .await
+}
+
+fn guessing_game() -> u32 {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("Guess the number!");
-    // println!("The secret number is :{secret_number}");
+    println!("the number is {secret_number}");
+
+    let mut attempts = 0;
 
     loop {
         println!("Please input your guess");
@@ -21,9 +64,11 @@ async fn main() -> std::io::Result<()> {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
+        attempts += 1;
+
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(err) => continue,
+            Err(_) => continue,
         };
 
         println!("You guessed: {guess}");
@@ -33,20 +78,9 @@ async fn main() -> std::io::Result<()> {
             Ordering::Greater => println!("Too high!"),
             Ordering::Equal => {
                 println!("Correct!");
-                break;
-            },
+                return attempts;
+                // break;
+            }
         }
     }
-
-    std::env::set_var("RUST_LOG", "debug");
-    std::env::set_var("RUST_BACKTRACE", "1");
-    env_logger::init();
-
-    HttpServer::new(move || {
-        let logger = Logger::default();
-        App::new().wrap(logger).service(get_task).service(get_task2)
-    })
-    .bind(("127.0.0.1", 80))?
-    .run()
-    .await
 }
